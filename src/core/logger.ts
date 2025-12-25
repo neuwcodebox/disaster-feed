fb945918eacd810910cb2462aea04b7912fc967a
neuwcodebox <neuwgmbox@gmail.com>
2025-12-25 16:36:18 +0900

    fix(logger): set logging level based on NODE_ENV

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,4 +1,5 @@
 import pino from 'pino';
+import { env } from './env';
 
-const logger = pino({ name: 'app' });
+const logger = pino({ name: 'app', level: env.NODE_ENV === 'development' ? 'debug' : 'info' });
 export { logger };
